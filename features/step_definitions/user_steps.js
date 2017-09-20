76feaf7cc56d13bed94ec050ad752cb5513de299
Jim Barrows <Jim.Barrows@gmail.com>
2017-09-20 12:05:50 -0500

    Regisration without a user_id is complete.

--- features/step_definitions/user_steps.js
+++ features/step_definitions/user_steps.js
@@ -112,10 +112,9 @@ defineSupportCode(function ({
 	});
 
 	Then('I will be given a message that says "The username is required"', function (callback) {
-		expect(this.result.error).to.not.be.ok;
-		expect(this.result.data).to.be.ok;
-		expect(this.result.data).to.be.an.instanceof(Array);
-		expect(this.result.data.length).to.be.equal(1);
+		expect(this.result.error).to.be.null;
+		expect(this.result.data).to.not.be.null;
+		console.log("this.result.data: ", this.result.data);
 		expect(this.result.data.register.path).to.be.equal("user_id");
 		expect(this.result.data.register.message).to.be.equal("User login id is required, and must be a valid email address");
 		callback();
@@ -144,7 +143,6 @@ defineSupportCode(function ({
 	});
 
 	Then('I am logged out', function (callback) {
-		console.log("I am logged out: ", this.result);
 		expect(this.result.error).to.not.be.ok;
 		expect(this.result.data).to.be.ok;
 		callback();
--- resolvers/register.js
+++ resolvers/register.js
@@ -1,6 +1,19 @@
 import jwt from "jsonwebtoken";
 
 export default function (obj, args, context, graphql) {
+	let {user_id, password} = args.newUser;
+	if (!user_id) {
+		return {
+			path   : "user_id",
+			message: "User login id is required, and must be a valid email address"
+		};
+	}
+	if (!password) {
+		return {
+			path   : "password",
+			message: "The password is required"
+		};
+	}
 	return context.e_commerce_db.one("insert into user_login(user_id, password) values($1, $2) returning id", [args.newUser.user_id, args.newUser.password])
 			.then(data => {
 
